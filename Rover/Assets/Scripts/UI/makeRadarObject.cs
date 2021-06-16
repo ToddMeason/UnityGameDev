67d9a8c77604f8833aaa90a72d8915a38e7f52df
ToddPoly <meast2@student.op.ac.nz>
2021-06-16 18:59:02 +1200

    Got radar implemented and working
    May need to slightly adjust angles later

---
+++ Rover/Assets/Scripts/UI/makeRadarObject.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class makeRadarObject : MonoBehaviour
+{
+    public Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Radar.RegisterRadarObject(this.gameObject, image);
+    }
+
+    private void OnDestroy() 
+    {
+        Radar.RemoveRadarObject(this.gameObject);
+    }
+}
