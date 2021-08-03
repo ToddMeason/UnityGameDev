0f125a1e58a5113f9a823ab03d68d2077f955017
ToddPoly <meast2@student.op.ac.nz>
2021-08-03 16:40:45 +1200

    Inventory displays to UI properly

--- Rover/Assets/Scripts/UI/DisplayInventory.cs
+++ Rover/Assets/Scripts/UI/DisplayInventory.cs
@@ -17,7 +17,7 @@ public class DisplayInventory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //UpdateDisplay();
+        UpdateDisplay();
     }
 
     public void CreateDisplay()
@@ -26,6 +26,24 @@ public class DisplayInventory : MonoBehaviour
         {
             var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
             obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+        }
+    }
+
+    public void UpdateDisplay()
+    {
+        for (int i = 0; i < inventory.Container.Count; i++)
+        {
+            if (itemsDisplayed.ContainsKey(inventory.Container[i]))
+            {
+                itemsDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+            }
+            else
+            {
+                var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+                obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+
+                itemsDisplayed.Add(inventory.Container[i], obj);
+            }
         }
     }
 }
