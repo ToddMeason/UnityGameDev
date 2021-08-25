8eec0d0691c6d06372332590f94420fc6fc9672d
ToddPoly <meast2@student.op.ac.nz>
2021-08-25 15:35:16 +1200

    Basic shopSlots setup for buying items in  the hub

--- Rover/Assets/ScriptableObjects/Items/Scripts/GroundItem.cs
+++ Rover/Assets/ScriptableObjects/Items/Scripts/GroundItem.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class GroundItem : MonoBehaviour
 {
     public ItemObject item;
+    public LayerMask terrainLayer;
 
     void OnTriggerEnter(Collider c)//Detecting ground collision
     {
-        if (c.tag == "Ground")
+        Debug.Log($"Hit {LayerMask.LayerToName(c.gameObject.layer)}");//This detects it is hitting tht correct layer and reading it 
+        if (c.tag == "Ground" || c.gameObject.layer == terrainLayer)//Dont know why it doesnt pick up layer like this will change later
         {
             GetComponent<Rigidbody>().Sleep();
         }
--- Rover/Assets/Scripts/Interactables/ShopSlot.cs
+++ Rover/Assets/Scripts/Interactables/ShopSlot.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chest : Interactable
+public class ShopSlot : Interactable
 {
     public GameObject[] items;//Add rarity later, maybe as another list 
+    public GameObject newItem;
     public Renderer renderer;
 
-    private void Start()//Need to set item in here later once rarity is setup
+    public int shopCount = 4;
+
+    void Start()//Need to set item in here later once rarity is setup
     {
         renderer = GetComponent<Renderer>();
         float rand = Random.Range(0, 100);
@@ -28,8 +31,16 @@ public class Chest : Interactable
             renderer.material.color = Color.black;
         }
 
+        DisplayItem();
         textPopup.autoSizeTextContainer = true;
         textPopup.text = "E\n" + cost;
+    }
+
+    public void DisplayItem()
+    {
+        newItem = Instantiate(items[Random.Range(0, items.Length)], new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), Quaternion.identity);//Displays item above the shopSlot
+        newItem.GetComponent<Rigidbody>().isKinematic = true;
+        newItem.GetComponent<GroundItem>().enabled = false;//Turns off components so it doesnt move and cant be picked up
     }
 
     public override void Interact()
@@ -37,10 +48,11 @@ public class Chest : Interactable
         if (!activated)
         {
             //Randomly pick an item from the items list then spawn it. Will add rarity later
-            GameObject newItem = Instantiate(items[Random.Range(0, items.Length)], new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), Quaternion.identity);//Pops item out above chest a little and throws it forward
-            newItem.GetComponent<Rigidbody>().AddForce(transform.forward * 200 + transform.up *200);
+            newItem.GetComponent<Rigidbody>().isKinematic = false;
+            newItem.GetComponent<GroundItem>().enabled = true;
+            newItem.GetComponent<Rigidbody>().AddForce(transform.forward * 200 + transform.up * 200);
 
-            activated = true;                       
+            activated = true;
         }
 
         base.Interact();
