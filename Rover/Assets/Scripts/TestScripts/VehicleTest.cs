59266905aaecf8523a68d03f3b88449ee7762ae7
Todd Meason (1000027339) <MEAST2@student.op.ac.nz>
2021-09-27 14:03:22 +1300

    Basic vehicle controller working

--- Rover/Assets/Scripts/TestScripts/VehicleTest.cs
+++ Rover/Assets/Scripts/TestScripts/VehicleTest.cs
@@ -7,19 +7,15 @@ public class VehicleTest : MonoBehaviour
 	float deadZone = 0f;
 	public float groundedDrag = 3f;
 	public float maxVelocity = 50;
-	public float hoverForce = 1000;
-	public float gravityForce = 1000f;
-	public float hoverHeight = 1.5f;
-	public GameObject[] hoverPoints;
 
 	public float forwardAcceleration = 8000f;
 	public float reverseAcceleration = 4000f;
-	float thrust = 0f;
+	[SerializeField] float thrust = 0f;
 
 	public float turnStrength = 1000f;
 	float turnValue = 0f;
 
-	//public ParticleSystem[] dustTrails = new ParticleSystem[2];
+	public ParticleSystem[] dustTrails = new ParticleSystem[2];
 
 	[SerializeField] LayerMask layerMask;
 
@@ -78,50 +74,15 @@ public class VehicleTest : MonoBehaviour
 
 	void FixedUpdate()
 	{
+		//add something to make emmission stop if not moving
+		var emissionRate = 10;
+		body.drag = groundedDrag;
 
-		//  Do hover/bounce force
-		RaycastHit hit;
-		bool grounded = false;
-		for (int i = 0; i < hoverPoints.Length; i++)
+		for (int i = 0; i < dustTrails.Length; i++)
 		{
-			var hoverPoint = hoverPoints[i];
-			if (Physics.Raycast(hoverPoint.transform.position, -Vector3.up, out hit, hoverHeight, layerMask))
-			{
-				body.AddForceAtPosition(Vector3.up * hoverForce * (1.0f - (hit.distance / hoverHeight)), hoverPoint.transform.position);
-				grounded = true;
-			}
-			else
-			{
-				// Self levelling - returns the vehicle to horizontal when not grounded and simulates gravity
-				if (transform.position.y > hoverPoint.transform.position.y)
-				{
-					body.AddForceAtPosition(hoverPoint.transform.up * gravityForce, hoverPoint.transform.position);
-				}
-				else
-				{
-					body.AddForceAtPosition(hoverPoint.transform.up * -gravityForce, hoverPoint.transform.position);
-				}
-			}
+			var emission = dustTrails[i].emission;
+			emission.rateOverTime = new ParticleSystem.MinMaxCurve(emissionRate);
 		}
-
-		var emissionRate = 0;
-		if (grounded)
-		{
-			body.drag = groundedDrag;
-			emissionRate = 10;
-		}
-		else
-		{
-			body.drag = 0.1f;
-			thrust /= 100f;
-			turnValue /= 100f;
-		}
-
-		//for (int i = 0; i < dustTrails.Length; i++)
-		//{
-		//	var emission = dustTrails[i].emission;
-		//	emission.rateOverTime = new ParticleSystem.MinMaxCurve(emissionRate);
-		//}
 
 		// Handle Forward and Reverse forces
 		if (Mathf.Abs(thrust) > 0)
@@ -137,7 +98,6 @@ public class VehicleTest : MonoBehaviour
 			body.AddRelativeTorque(Vector3.up * turnValue * turnStrength);
 		}
 
-		Debug.Log(thrust);
 
 		// Limit max velocity
 		if (body.velocity.sqrMagnitude > (body.velocity.normalized * maxVelocity).sqrMagnitude)
