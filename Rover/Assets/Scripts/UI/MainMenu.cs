7c9be3e351281fa1f23e3542bbdaefda5da60b81
ToddPoly <meast2@student.op.ac.nz>
2021-11-17 16:24:34 +1300

    Main Menu almost setup

---
+++ Rover/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu, controlsMenu, objectiveMenu;
+
+    public GameObject mainFirstButton, controlsFirstButton, objectiveFirstbutton;
+
+
+    void Start()
+    {
+        OpenMainMenu();
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+
+    }
+
+    public void OpenMainMenu()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(mainFirstButton);
+    }
+
+    public void OpenControls()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(controlsFirstButton);
+    }
+
+    public void OpenObjective()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(objectiveFirstbutton);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
